49456ede89b6e014dc0575a32ebe44207a3c372d
gravityfall-007 <krishnakevin007@gmail.com>
2023-03-05 10:43:45 +0100

    setup and required packages commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function takes lists of packages from the requirements file
+
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+
+
+setup(
+name='MLProject',
+version='0.0.1',
+author='Krishna Oli',
+author_email='krishankevin007@gmail.com',
+packages=find_packages(),
+install_requirements=get_requirements('requirements.txt')
+
+)
